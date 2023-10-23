f39a92acbcf32b60d6955e91f1d04037188dc86a
mahdiar <mahdiarrezvani59@gmail.com>
2023-10-23 05:17:48 -0700

    day nine

--- database/dataBaseChallenges.js
+++ database/dataBaseChallenges.js
@@ -4,10 +4,10 @@ export const dataBase = {
     code: [
         {
             infoChallenge: { start: '1402/07/29', end: '1402/08/05', period: 6, target: 1200 },
-            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [380] },
-            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [19] },
-            abbas: { name: 'amir abbas', state: '👎', result: 0, arrayChallengeProcess: [110] },
-            mahdi: { name: 'amir mahdi', state: '👎', result: 0, arrayChallengeProcess: [87] },
+            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [380, 148] },
+            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [19, 95] },
+            abbas: { name: 'amir abbas', state: '👎', result: 0, arrayChallengeProcess: [110, 150] },
+            mahdi: { name: 'amir mahdi', state: '👎', result: 0, arrayChallengeProcess: [87, 60] },
         },
     ],
     //!                                         pushUp
@@ -15,10 +15,10 @@ export const dataBase = {
     pushUp: [
         {
             infoChallenge: { start: '1402/07/29', end: '1402/08/05', period: 7, target: 200 },
-            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [30] },
-            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [30] },
-            abbas: { name: 'amir abbas', state: '👎', result: 0, arrayChallengeProcess: [10] },
-            mahdi: { name: 'amir mahdi', state: '👎', result: 0, arrayChallengeProcess: [0] },
+            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [30, 0] },
+            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [30, 35] },
+            abbas: { name: 'amir abbas', state: '👎', result: 0, arrayChallengeProcess: [10, 10] },
+            mahdi: { name: 'amir mahdi', state: '👎', result: 0, arrayChallengeProcess: [0, 0] },
         },
     ],
 }
