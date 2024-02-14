a7473a4d58f1cb44bdbb8242bd3440b54b2cad37
mahdiar <mahdiarrezvani59@gmail.com>
2024-02-14 08:53:54 -0800

    120 to 123

--- js/dataBase.js
+++ js/dataBase.js
@@ -2,7 +2,7 @@ import { month1Mahdiar, month1Amin, month1Abbas, month1AmirMahdi } from '../data
 import { month2Mahdiar, month2Amin, month2Abbas ,month2AmirMahdi} from '../database/month2.js'
 import { month3Mahdiar, month3Amin, month3Abbas ,month3AmirMahdi} from '../database/month3.js'
 import { month4Mahdiar, month4Amin, month4Abbas ,month4AmirMahdi} from '../database/month4.js'
-// import { month5Mahdiar, month5Amin, month5Abbas ,month5AmirMahdi} from '../database/month5.js'
+import { month5Mahdiar, month5Amin, month5Abbas ,month5AmirMahdi} from '../database/month5.js'
 // import { month6Mahdiar, month6Amin, month6Abbas ,month6AmirMahdi} from '../database/month6.js'
 export let dataBase = [
     // Mahdiar
@@ -11,7 +11,7 @@ export let dataBase = [
         ...month2Mahdiar,
         ...month3Mahdiar,
         ...month4Mahdiar,
-        // ...month5Mahdiar,
+        ...month5Mahdiar,
         // ...month6Mahdiar,
     ],
     // Amin
@@ -20,7 +20,7 @@ export let dataBase = [
         ...month2Amin,
         ...month3Amin,
         ...month4Amin,
-        // ...month5Amin,
+        ...month5Amin,
         // ...month6Amin,
     ],
     // Amir Abbas
@@ -29,7 +29,7 @@ export let dataBase = [
         ...month2Abbas,
         ...month3Abbas,
         ...month4Abbas,
-        // ...month5Abbas,
+        ...month5Abbas,
         // ...month6Abbas,
     ],
     [
@@ -37,7 +37,7 @@ export let dataBase = [
         ...month2AmirMahdi,
         ...month3AmirMahdi,
         ...month4AmirMahdi,
-        // ...month5AmirMahdi,
+        ...month5AmirMahdi,
         // ...month6AmirMahdi,
     ],
 ];
